5e3d6ad5be6d4ae95eca5c42e38c0a8d4cf85998
Cristian Cajiao <cristiancajiaos@outlook.com>
2024-10-27 20:26:57 -0300

    Creada estructura de la página sin colores aplicados

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 var date = new Date();
 var currentYearEl = document.getElementById('currentYear');
 currentYearEl.innerHTML = date.getFullYear();
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
